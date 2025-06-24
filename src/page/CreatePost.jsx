413e2f93f41ff4f7bcf55a6a436cb2162907c79f
unknown <cb010931@student.apiit.lk>
2025-06-24 23:57:32 +0530

    first

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/dalle', {
+        const response = await fetch('https://corsproxy.io/?url=https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -57,7 +57,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/post', {
+        const response = await fetch('https://corsproxy.io/?url=https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/post', {
+      const response = await fetch('https://corsproxy.io/?url=https://vercel.com/codict008ovindugmailcoms-projects/dall-e-backend/FREo7wvrxVCnCWZFKUxneQ1ighUr/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
